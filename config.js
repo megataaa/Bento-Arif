9323f0fe57de4a2f1e5aafe7561531f4b0850501
megataaa <103764715+megataaa@users.noreply.github.com>
2024-08-01 12:59:28 +0800

    Update config.js (changed button links)

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const CONFIG = {
 			id: '2',
 			name: 'Mail',
 			icon: 'mail',
-			link: 'https://mail.protonmail.com/',
+			link: 'https://mail.google.com/mail/u/0/#inbox',
 		},
 		{
 			id: '3',
@@ -77,15 +77,15 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
+			name: 'The Star',
 			icon: 'glasses',
-			link: 'https://reddit.com',
+			link: 'https://www.thestar.com.my/',
 		},
 		{
 			id: '6',
-			name: 'Odysee',
+			name: 'youtube',
 			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://www.youtube.com/',
 		},
 	],
 
